fef5288038e98e527ef21494523ebb722293c536
NHoeller <Nicolas.Hoeller@gmx.eu>
2015-01-29 08:43:28 +0100

    Create modula.outro.js

---
+++ src/modula.outro.js
@@ -0,0 +1,15 @@
+// <-- insert source files here
+
+  /**
+   * set modula to context
+   */
+  window.modula = modula;
+  
+  
+  /**
+   * return the modula
+   */
+  return modula;
+
+
+})( window );
