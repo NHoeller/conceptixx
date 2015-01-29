37cf81367dd233e30780c3f50125fbc2e84a1feb
NHoeller <Nicolas.Hoeller@gmx.eu>
2015-01-29 08:41:10 +0100

    Create modula.intro.js

---
+++ src/modula.intro.js
@@ -0,0 +1,35 @@
+/**
+ * modula intro
+ */
+( function( context , undefined ) {
+
+  /**
+   * define some variables
+   */
+  var
+
+
+  /**
+   * define Array functions
+   *
+   * @dependency : simplify
+   */
+   var
+   // Arr = [],
+   push = simplify( Array.prototype.push ), // same as Arr.push
+   slice = simplify( Array.prototype.slice ), // same as Arr.slice
+   concat = simplify( Array.prototype.concat ); // same as Arr.concat
+   
+   
+   /**
+    * define some Object functions
+    *
+    * @dependency : simplify
+    */
+    var
+    // Obj = {},
+    toString = simplify( Object.prototype.toString ), // same as Obj.toString
+    hasOwnProperty = simplify( Object.prototype.hasOwnProperty ); // same as Obj.hasOwnProperty
+    
+    
+// --> insert src files here
