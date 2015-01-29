5aa074d8b3dd0e60d36a2e2147c3742468d04c0e
NHoeller <Nicolas.Hoeller@gmx.eu>
2015-01-29 13:06:00 +0100

    Create _.js

---
+++ src/_.js
@@ -0,0 +1,14 @@
+/*
+ * modula is currently not using grunt or something else to
+ * automatically build a development file so in here will be
+ * listed all files that have to be combined to get the
+ * modula.js file
+ *
+ * @status : uncomplete
+ */
+var BUILDER =
+  [
+    "src/modula.intro.js",
+    "src/modula.modula.js",
+    "src/modula.outro.js"
+  ];
