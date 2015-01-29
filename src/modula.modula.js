af00afad0e20641908ffa0ee24dd5709e5726dc5
NHoeller <Nicolas.Hoeller@gmx.eu>
2015-01-29 12:59:45 +0100

    Update modula.modula.js

--- src/modula.modula.js
+++ src/modula.modula.js
@@ -1,14 +1,14 @@
   /**
    * modula function
    *
-   * @dependency : core , modula.core.init , modula.extended.init
+   * @dependency : modulaCore , modula.core.init , modula.extended.init
    */
   var
   modula = function( selector , context ) {
     // check for empty selector (null, false, undefined and '')
     if( !selector ) {
       // return core object (basic functions)
-      return core || new modula.core.init();
+      return modulaCore || new modula.core.init();
     }
     // if we have a selector return extended object
     return new modula.extended.init( selector , context );
