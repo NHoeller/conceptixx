5ecc6b694abc763d54153ebea5569cfd72f3d778
NHoeller <NHoeller@macbooknicowlan.fritz.box>
2015-01-29 20:53:40 +0100

    further updates and new files

---
+++ inProgress/_.js
@@ -0,0 +1,15 @@
+/**
+ * @project : modula.js
+ * @package : xxx
+ * @internal : xxx
+ * @type : xxx
+ * @dependencies : none
+ *
+ * @description :
+ * enter description here ...
+ * 
+ * @example :
+ * 
+ * @notice :
+ * 
+ */
--- src/_.js
+++ src/_.js
@@ -7,10 +7,25 @@
  * @status : uncomplete
  */
 var BUILDER =
-  [
-    "src/modula.intro.js",
-    "src/modula.modula.js",
-    "src/modula.modula.core.js",
-    "src/modula.modula.extended.js",
-    "src/modula.outro.js"
-  ];
+	[
+		"src/modula.intro.js",
+		"src/modula.simplify.js",
+		"src/modula.indexOf.js",
+		"src/modula.isArray.js",
+		"src/modula.isArraylike.js",
+		"src/modula.isBool.js",
+		"src/modula.isCollection.js",
+		"src/modula.isEqual.js",
+		"src/modula.isInstance.js",
+		"src/modula.isNative.js",
+		"src/modula.isNode.js",
+		"src/modula.isPlainObject.js",
+		"src/modula.isScion.js",
+		"src/modula.isSibling.js",
+		"src/modula.isWindow.js",
+		"src/modula.Support.js",
+		"src/modula.modula.js",
+		"src/modula.modula.core.js",
+		"src/modula.modula.extended.js",
+		"src/modula.outro.js"
+	];
