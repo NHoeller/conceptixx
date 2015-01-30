012818a28d47b3406e0ba0946864abf5c29322ce
NHoeller <Nicolas.Hoeller@gmx.eu>
2015-01-30 08:37:33 +0100

    Update modula.Strings.js with Propertizer

--- inProgress/modula.select.Strings.js
+++ inProgress/modula.select.Strings.js
@@ -3,7 +3,7 @@
  * @package : select
  * @internal : Strings
  * @type : object
- * @dependencies : none
+ * @dependencies : Propertizer
  *
  * @description :
  * in 'Strings' are several used Strings for the 'select' engine
@@ -13,8 +13,8 @@
 	/**
 	 * define string expressions for the selector engine
 	 */
-	Strings = {
+	Strings = Propertizer( "Strings" , {
 		bools : "checked|selected|async|autofocus|autoplay|" +
 			"controls|defer|disabled|hidden|ismap|loop|" +
 			"multiple|open|readonly|required|scoped",
-	};
+	} );
