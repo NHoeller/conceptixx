660e577df1991369e7165b71a385cb146ebbdcb4
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2015-02-10 14:10:27 +0100

    Update DEMAND.js

--- inProgress/tasks/ready/DEMAND.js
+++ inProgress/tasks/ready/DEMAND.js
@@ -34,29 +34,29 @@ console.log( task , type , args , last( PushStack[ task.PushStack ] ) );
 				// define ready object
 				var ready = ReadyHandler[ type ];
 				/**
-				 * check for DEMAND Stack in ready
+				 * check for DEMAND in task's ReadyStates and in ready
 				 */
-				if( !ready[ ' '+args[ 0 ] ] ) {
-					// set ready for args[ 0 ]
-					ready[ ' '+args[ 0 ] ] = [];
-				}
+				if( task ) {
+					task.ReadyStates[ type ] = task.ReadyStates[ type ] || {};
+					ready[ ' '+args[ 0 ] ] = ready[ ' '+args[ 0 ] ] || { count : 0 };
+				};
 				/**
 				 * if we have a not ready task
 				 */
-				if( task && task.ReadyStates[ type ] && !task.ReadyStates[ type ][ args[ 0 ] ] ) {
-					// add task's PushStack to ready for completition
-					ready[ ( ready.count = ready.count >>> 0 ) ] = task;
+				if( task && !task.ReadyStates[ type ][ args[ 0 ] ] ) {
+					// add task to ready for completition
+					ready[ ' '+args[ 0 ] ][ ready[ ' '+args[ 0 ] ].count ] = task;
 					// increase count for next turn
-					ready.count++;
+					ready[ ' '+args[ 0 ] ].count++;
 
 					// remove appended function if needed
 					if( last( PushStack[ task.PushStack ] ) === args[ 1 ] ) { task.remove(); }
 
 					// add readyTrigger to task's Stack
 					PushStack[ task.PushStack ].push( [ function(){ return task.ReadyStates[ type ][ args[ 0 ] ]; } ] );
-					// set ReadyStates for SELECT ready
+					// set ReadyStates for DEMAND ready
 					task.ReadyStates[ type ][ args[ 0 ] ] = UseStates.progress;
-					// bind SELECT to trigger;
+					// bind DEMAND to trigger;
 					task.trigger[ type ] = true;
 					// add function to task (if set)
 					if( args[ 1 ] ) { task.append( args[ 1 ] , args[ 2 ] ); }
@@ -67,27 +67,26 @@ console.log( task , type , args , last( PushStack[ task.PushStack ] ) );
 			/**
 			 * define completed
 			 */
-			completed : function( task ,args ) {
-				// check if we have args and args are set to 
-				
+			completed : function( task , args ) {
+				// define ready object
+				var ready = ReadyHandler[ 'DEMAND' ];
 				// define some variables
 				var i = 0 , task;
 				// loop the counts
-				while( ready[ i ] !== undefined ) {
+				while( ready[ ' '+args ][ i ] !== undefined ) {
 					// get task from ready[ i ]
-					task = ready[ i ];
+					task = ready[ ' '+args ][ i ];
 					// set readyState as complete for this task
-					task.ReadyStates[ 'DOM' ] = UseStates.complete; 
-					// execute task
-					tasks.resolve( task ); 
+					task.ReadyStates[ 'DEMAND' ] = UseStates.complete; 
+					// execute task by setting timeout
+					window.setTimeout( function() { tasks.resolve( task ); } ); 
 					// delete ready[ i ] in fact of execution and increase counter
-					delete ready[ i++ ];
+					delete ready[ ' '+args ][ i++ ];
 				};
 				// reset ready.count
 				ready.count = 0;
 				// we are done sucessfully
 				return true;
-				
 			}
 
 
