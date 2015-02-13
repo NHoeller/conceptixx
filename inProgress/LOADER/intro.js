769af637a96adb639327eb7b1dca7d07b0f87537
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2015-02-13 07:13:14 +0100

    Create intro.js

---
+++ inProgress/LOADER/intro.js
@@ -0,0 +1,18 @@
+/**
+ * @project : modula.js
+ * @package : loader
+ * @internal : loader.intro
+ * @type : constructor
+ * @dependencies : none
+ *
+ * @description :
+ * the main part of the modula is a self extending loader object
+ */
+
+
+	/**
+	 * init modula by an anonymous function
+	 */
+	( function() {
+
+
