b941c04891e53020d1687094f85fac7d42907a77
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2015-02-13 12:45:19 +0100

    Create define.js

---
+++ inProgress/LOADER/define.js
@@ -0,0 +1,32 @@
+/**
+ * @project : modula.js
+ * @package : loader
+ * @internal : loader.intro
+ * @type : constructor
+ * @dependencies : none
+ *
+ * @description :
+ * the main part of the modula is a self extending loader object
+ */
+
+
+		/**
+		 * pre init
+		 */
+		// get the primary script tag of the modula
+		var Script = document.getElementById( 'modulajs' );
+		// define HashTag
+		var HashTag
+		// check if we have primary script and get HashTag
+		if( Script && ( HashTag = Script.dataset || Script.title || Script.class ) {
+// todo
+		}
+
+
+		/**
+		 * init modula by an anonymous function
+		 */
+		( function( context ) {
+		}
+
+
