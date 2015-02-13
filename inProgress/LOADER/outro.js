f93d4075e5ad8f1e981a84e2d1036e41c9df3158
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2015-02-13 07:16:14 +0100

    Create outro.js

---
+++ inProgress/LOADER/outro.js
@@ -0,0 +1,15 @@
+/**
+ * @project : modula.js
+ * @package : loader
+ * @internal : loader.outro
+ * @type : constructor
+ * @dependencies : none
+ *
+ * @description :
+ * the main part of the modula is a self extending loader object
+ */
+
+
+	)( window , undefined );
+
+
