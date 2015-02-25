a6b15a18a0b087e1f4dcf18f82ff8896cda3e637
Nicolas Hoeller <Nicolas.Hoeller@gmx.eu>
2015-02-25 12:58:35 +0100

    Update and rename src/Support.g to src/Support.g/UrlObject.js

---
+++ src/Support.g/UrlObject.js
@@ -0,0 +1,12 @@
+/**
+* @project : modula.js
+* @package :
+* @internal : Support.UrlObject
+* @type : function
+* @dependencies : none
+*/
+		Defaults(
+			false ,
+			[ 'Support' , 'UrlObject' ] ,
+			typeof URL === 'function'
+		);
